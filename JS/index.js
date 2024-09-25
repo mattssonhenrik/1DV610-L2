f50e04da809437a9c8898f77a769c579d2eb3540
Henrik Mattsson <hm222yj@student.lnu.se>
2024-09-25 16:41:09 +0200

    Fix the small bug of value being kept/read only once
    Rewrote the reference to the html-form-input-element which caused the next event-listener to not having the correct reference to the html-input-element.

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,17 @@
-import { formInput } from "./inputFromBrowser/inputFromBrowser.js"
+// import { formInput } from "./inputFromBrowser/inputFromBrowser.js"
 
 let isInputInCorrectformat = true
+
+let formInputValueReference = document.querySelector("input")
+let formInputValue = ''
+export let formInput = document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault()
+    formInputValue = formInputValueReference.value
+    console.log(formInputValue)
+    console.log("did this formInput fire?")
+    // formInputValue = ''
+})
+
 console.log("index.js has fired")
 console.log("Well now inputForm is firing from index.js" + formInput)
+

854d88082cba3cabf6c87d072262bf45ab9b3d7a
Henrik Mattsson <hm222yj@student.lnu.se>
2024-09-30 01:35:49 +0200

    Add dynamic text according to which rule is in effect
    Added a basic class to insert a textcontent for the rule that is applied.

--- JS/browserDisplayOutput/browserDisplayOutput.js
+++ JS/browserDisplayOutput/browserDisplayOutput.js
@@ -1,3 +1,29 @@
 // browserDisplayOutput.js
 // Author Henrik Mattsson
 
+import { RuleForInput } from '../ruleForInput/ruleForInput.js'
+
+export class BrowserDisplayOutput {
+    constructor() {
+        this.textField = document.querySelector('#text-container')
+        this.ruleHandler = new RuleForInput()
+        // this.currentRule = this.ruleHandler.getChosenRule()
+
+        window.addEventListener('load', () => {
+            this.checkRules()
+        })
+    }
+
+
+    checkRules() {
+        if (this.ruleHandler.lowerAndUpperLettersPlusNumbers) {
+            this.textField.textContent = 'Uppercase, lowercase and numbers are valid'
+        } else if (this.ruleHandler.lowerLetters) {
+            this.textField.textContent = 'Only lowercase letters'
+        } else if (this.ruleHandler.upperLetters) {
+            this.textField.textContent = 'Only uppercase letters'
+        } else {
+            this.textField.textContent = 'Only number (0-9)'
+        }
+    }
+}
--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,10 @@
 // Author: Henrik Mattsson
 
 import { InputProcessor } from './processInput/processInput.js'
+import { BrowserDisplayOutput } from './browserDisplayOutput/browserDisplayOutput.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     const inputProcessor = new InputProcessor()
+    const browserDisplay = new BrowserDisplayOutput()
+    console.log(browserDisplay.currentRule)
 })
